3e0c068e328bc242fda78eef776d15950938e15b
RichardEsquivel <rlesquivel@gmail.com>
2020-04-14 10:28:32 -0500

    Starting directory

---
+++ scratch.py
@@ -0,0 +1,11 @@
+a = ['Waltz', 'Tango', 'Viennese Waltz', 'Foxtrot',
+     'Cha Cha', 'Samba', 'Rumba', 'Paso Doble', 'Jive']
+
+
+def sort_alpha(arr):
+    cool = sorted(arr)
+    for i in cool:
+        print(i)
+
+
+sort_alpha(a)
